ebc5debdb47ae7c6baf29b67b0d9115a5cf9cb14
theoleary <eoin1@olearyusa.com>
2018-01-11 09:13:09 -0800

    new

---
+++ src/graphics/test.java
@@ -0,0 +1,29 @@
+package graphics;
+
+	import java.awt.*;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;  
+
+
+		public class test{ 
+		      
+			test()  
+	        {  
+	        JFrame f= new JFrame("Panel Example");    
+	        JPanel panel=new JPanel();  
+	        panel.setBounds(40,80,200,200);    
+	        panel.setBackground(Color.gray);  
+	        f.add(panel);  
+	                f.setSize(400,400);    
+	                f.setLayout(null);    
+	                f.setVisible(true);    
+	        }  
+	        public static void main(String args[])  
+	        {  
+	        new test();  
+	        }  
+		  
+		}  
+
+
