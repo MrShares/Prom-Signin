279b08fe5cfcf419432be29da59a353c0461b2b0
Jakob <allen.jake66@gmail.com>
2018-01-11 09:10:06 -0800

    Not finished
    N

---
+++ Student.java
@@ -0,0 +1,44 @@
+/**
+ * Contains All Methods For Student
+ * 
+ * @author (Jakob Allen) 
+ * @version (1/11/18)
+ */
+public class Student 
+{//Student Class Start
+	
+	private String LName;
+	private String FName;
+	private int studID;
+	private boolean inside = false;
+	
+public Student(String LName, String FName, int studID, boolean inside) 
+{//Student Method Start
+this.LName = LName;
+this.FName = FName;
+this.studID = studID;
+this.inside = inside;
+}//Student Method End
+
+public String getName()
+{//getName Method Start
+	String check = FName + " " + LName;
+	return check;
+}//getName Method End
+
+<<<<<<< HEAD
+public void setName(String newLName, String newFName)
+{
+this.LName = newLName;
+this.FName = newFName;
+}//setName Method End
+
+public boolean getInside()
+{//getInside Method Start
+boolean check = this.inside;
+return check;
+}//getInside Method End
+}//Student Class End
+=======
+}
+>>>>>>> 03e04931f75224b66f3cb44529a2f544528c16cb
