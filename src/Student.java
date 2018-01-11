fff1bf319c02abf47741646202215b0333be2691
Jakob <allen.jake66@gmail.com>
2018-01-11 09:03:18 -0800

    Student Codded
    Still Not Finished

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,40 @@
+/**
+ * Contains All Methods For Student
+ * 
+ * @author (Jakob Allen) 
+ * @version (1/11/18)
+ */
+public class Student 
+{//Student Class Start
+	
+	private String LName;
+	private String FName;
+	private int studID;
+	private boolean inside = false;
+	
+public Student(String LName, String FName, int studID, boolean inside) 
+{//Student Method Start
+this.LName = LName;
+this.FName = FName;
+this.studID = studID;
+this.inside = inside;
+}//Student Method End
 
-public class Student {
+public String getName()
+{//getName Method Start
+	String check = FName + " " + LName;
+	return check;
+}//getName Method End
 
-}
-//idasnfiuosdanifmweiuonvianinmfms
+public void setName(String newLName, String newFName)
+{
+this.LName = newLName;
+this.FName = newFName;
+}//setName Method End
+
+public boolean getInside()
+{//getInside Method Start
+boolean check = this.inside;
+return check;
+}//getInside Method End
+}//Student Class End
