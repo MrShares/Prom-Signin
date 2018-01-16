dd1c3e23cf2754b590f7da41d236366cdaa7cd25
THENATHAN <nemontanez@gmail.com>
2018-01-16 08:02:15 -0800

    hey

--- src/Directory.java
+++ src/Directory.java
@@ -1,13 +1,19 @@
 import java.util.*;
 public class Directory {
 	
- private ArrayList<Student> myDanceList;
+ private ArrayList<Student> myDanceList = new ArrayList();
  
- public Directory() { //giving me a blank directory to work w/
+ public Directory() {//giving me a blank directory to work w/
+	 
  }
  
  public void addStudent(Student n){ //adds student to the list
  myDanceList.add(n);
  }
  
+ public String toString() {
+	 for(int i = 0; i < myDanceList.size(); i++) {
+		 System.out.println()
+	 }
+ }
 }
